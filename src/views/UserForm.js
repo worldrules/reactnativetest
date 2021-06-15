25f1800d2a2bd3178e5f2777767dc76eac005b3e
worldrules <worldrulesdev@gmail.com>
2021-06-15 12:42:31 -0300

    start

---
+++ src/views/UserForm.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import { Text } from 'react-native'
+
+export default props => {
+    return (
+        <div>
+            <Text>UserForm</Text>
+        </div>
+    )
+}
